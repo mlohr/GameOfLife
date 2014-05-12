6d891ba3a098e8e1c03a2c41e367a4f9f609c874
Michel Löhr <michel.lohr@gmail.com>
2014-05-12 09:48:41 +0200

    initial commit

---
+++ Life/src/gameoflife/GameOfLifeTest.java
@@ -0,0 +1,83 @@
+package gameoflife;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameOfLifeTest {
+	
+	private static final String NEW_LINE = " ";
+	private static final String ALIVE_CELL = "x";
+	private static final String DEAD_CELL = ".";
+	
+	public void assertNextIteration(String current, String expected) {
+		assertEquals(expected, nextIteration(current));
+	}
+
+	@Test
+	public void nullTest() {
+		assertNextIteration(null, "");
+	}
+
+	@Test
+	public void deadCellsStayDead() throws Exception {
+		assertNextIteration(DEAD_CELL, DEAD_CELL);
+		assertNextIteration("...", "...");
+		assertNextIteration("... ...", "... ...");
+	}
+
+	@Test
+	public void testIterations() throws Exception {
+		assertNextIteration(ALIVE_CELL, DEAD_CELL);
+		assertNextIteration("xxx", ".x.");
+		assertNextIteration("xxxx", ".xx.");
+		assertNextIteration("xxx.xxx", ".x...x.");
+		assertNextIteration("xx xx", "xx xx");
+		assertNextIteration("... xxx ...", ".x. .x. .x.");
+		assertNextIteration(".x. .x. .x.", "... xxx ...");
+	}
+	
+	private String nextIteration(String iteration) {
+		String result = "";
+		if (iteration == null) 
+			return result;
+		final String[] lines = iteration.split(NEW_LINE);
+		for (int y=0; y < lines.length; y++)	{			
+			for(int x=0; x < lines[y].length(); x++) {
+				result += cellWillLive(lines, x, y) ? ALIVE_CELL : DEAD_CELL;
+			}
+			result += NEW_LINE;
+		}
+		return result.trim();
+	}
+
+	public boolean cellWillLive(String[] lines, int x, int y) {
+		int neighbours = numberOfLiveNeighbours(lines, x, y);
+		if (cellIsAlive(lines[y], x))
+			return neighbours > 2;		
+		else
+			return neighbours == 3;
+	}
+
+	public int numberOfLiveNeighbours(String[] lines, int x, int y) {
+		int neighbours = countLiveNeighboursInRow(lines, x, y);
+		if (y>0)
+			neighbours += countLiveNeighboursInRow(lines, x, y-1);
+		if (y<lines.length-1) {
+			neighbours += countLiveNeighboursInRow(lines, x, y+1);
+		}
+		return neighbours;
+	}
+
+	public int countLiveNeighboursInRow(String[] lines, int x, int y) {
+		int neighbours = cellIsAlive(lines[y], x) ? 1 : 0;
+		neighbours += x>0 && cellIsAlive(lines[y], x-1) ? 1 : 0;
+		neighbours += x<lines[y].length()-1 && cellIsAlive(lines[y], x+1) ? 1 : 0;
+		return neighbours;
+	}
+
+	public boolean cellIsAlive(String field, int i) {
+		return field.substring(i, i+1).equals(ALIVE_CELL);
+	}
+
+}
